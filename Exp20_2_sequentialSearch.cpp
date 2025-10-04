9a44444d81c051dcac92df8a5fcebf2286e63c68
Papri <papri.sen2020@gmail.com>
2025-10-04 23:53:47 +0530

    Create Exp20_2_sequentialSearch.cpp

---
+++ Exp20_2_sequentialSearch.cpp
@@ -0,0 +1,40 @@
+//Papri Sen
+//24070123070
+//A3
+#include <iostream>
+using namespace std;
+int main() {
+    int n, key;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Enter element to search: ";
+    cin >> key;
+    
+    bool found = false;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            cout << "Element found at index " << i << endl;
+            found = true;
+            break;
+        }
+    }
+    
+    if(!found) {
+        cout << "Element not found in the array." << endl;
+    }
+
+    return 0;
+}
+/*
+Output:
+Enter number of elements: 6
+Enter elements: 4 5 1 3 7 9
+Enter element to search: 3
+Element found at index 3
+*/
